f0e3d78fb98c5ab95e26598951708aca8e48e43a
Albert Huynh <albert.huynh@ibm.com>
2025-07-05 16:24:54 -0400

    create application types

---
+++ src/app/types/applications/index.ts
@@ -0,0 +1,133 @@
+import { db } from "@/lib/firebase";
+import { uploadFile } from "@/lib/storage";
+import {
+  addDoc,
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  serverTimestamp,
+  Timestamp,
+  updateDoc,
+} from "firebase/firestore";
+
+export type ApplicationType = {
+  id: string;
+  name: string;
+  email: string;
+  quesitons: Record<string, string>;
+  status: "pending" | "accepted" | "rejected";
+  createdAt: Timestamp;
+  updatedAt: Timestamp;
+};
+
+export class Application implements ApplicationType {
+  id: string;
+  name: string;
+  email: string;
+  quesitons: Record<string, string>;
+  status: "pending" | "accepted" | "rejected";
+  createdAt: Timestamp;
+  updatedAt: Timestamp;
+
+  constructor(data: ApplicationType) {
+    this.id = data.id;
+    this.name = data.name;
+    this.email = data.email;
+    this.quesitons = data.quesitons;
+    this.status = data.status;
+    this.createdAt = data.createdAt || (serverTimestamp() as Timestamp);
+    this.updatedAt = data.updatedAt || (serverTimestamp() as Timestamp);
+  }
+
+  static converter = {
+    toFirestore: (application: Application) => {
+      return {
+        name: application.name,
+        email: application.email,
+        quesitons: application.quesitons,
+        status: application.status,
+        createdAt: application.createdAt,
+        updatedAt: serverTimestamp(),
+      };
+    },
+    fromFirestore: (snapshot: any, options: any) => {
+      const data = snapshot.data(options);
+      return new Application({
+        id: snapshot.id,
+        name: data.name,
+        email: data.email,
+        quesitons: data.quesitons,
+        status: data.status,
+        createdAt: data.createdAt,
+        updatedAt: data.updatedAt,
+      });
+    },
+  };
+
+  async create(positionId: string): Promise<string> {
+    const docRef = await addDoc(
+      collection(db, "applications", positionId).withConverter(
+        Application.converter
+      ),
+      this
+    );
+    this.id = docRef.id;
+    return this.id;
+  }
+
+  static async read(
+    positionId: string,
+    userId: string
+  ): Promise<Application | null> {
+    const docSnap = await getDoc(
+      doc(db, "applications", positionId, userId).withConverter(
+        Application.converter
+      )
+    );
+    if (docSnap.exists()) {
+      return docSnap.data();
+    }
+    return null;
+  }
+
+  static async readAll(positionId: string): Promise<Application[]> {
+    const querySnapshot = await getDocs(
+      collection(db, "applications", positionId).withConverter(
+        Application.converter
+      )
+    );
+    return querySnapshot.docs.map((doc) => doc.data());
+  }
+
+  async update(): Promise<void> {
+    const converter = Application.converter;
+    await updateDoc(
+      doc(db, "applications", this.id),
+      converter.toFirestore(this)
+    );
+  }
+
+  async updateStatus(
+    status: "pending" | "accepted" | "rejected"
+  ): Promise<void> {
+    this.status = status;
+    await this.update();
+  }
+
+  async delete() {
+    // should not be able to delete ANY application for historical reasons
+    return;
+  }
+
+  async uploadFile(
+    file: File,
+    positionId: string,
+    userId: string,
+    filename: string
+  ): Promise<string> {
+    const filePath = `applications/${positionId}/${userId}/${filename}`;
+    const { downloadURL } = await uploadFile(file, filePath);
+    return downloadURL;
+  }
+}
