aa11321e899c224bdb6510c1a07c27568e41c1af
Albert Huynh <itsalbert.huynh@mail.utoronto.ca>
2025-12-29 22:21:58 -0500

    remove light pillar way too laggy

--- buildwithai/app/page.tsx
+++ buildwithai/app/page.tsx
@@ -8,27 +8,12 @@ import Speakers from "@buildwithai/sections/Speakers";
 import Sponsors from "@buildwithai/sections/Sponsors";
 import WorkshopsPreview from "@buildwithai/sections/WorkshopsPreview";
 
-const LightPillar = dynamic(() => import("@buildwithai/components/LightPillar"), {
-  ssr: false,
-  loading: () => <div className="fixed inset-0 z-0 bg-black" />,
-});
 
 export default function Home() {
   return (
     <main className="relative min-h-screen bg-black">
       {/* Light Pillars Background - Fixed across entire page */}
       <div className="fixed inset-0 z-0">
-        <LightPillar
-          topColor="#6366F1"
-          bottomColor="#A855F7"
-          intensity={1}
-          pillarRotation={25}
-          glowAmount={0.002}
-          pillarWidth={3}
-          pillarHeight={0.4}
-          rotationSpeed={0.3}
-          noiseIntensity={0.5}
-        />
       </div>
 
       {/* Content */}
